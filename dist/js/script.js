929e74d6fae201b402b67afcd8856eccaf83cfd7
anelka ganteng <alanelka42@gmail.com>
2023-04-07 22:20:58 +0700

    add darkmode

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,8 +10,48 @@ hamburger.addEventListener("click", () => {
 window.onscroll = () => {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.getElementById("toTop");
 
-  window.pageYOffset > fixedNav
-    ? header.classList.add("navbar-fixed")
-    : header.classList.remove("navbar-fixed");
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
+  }
 };
+
+// click outside hamburger
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// checkbox conditions
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  document.documentElement.classList.remove("dark");
+  darkToggle.checked = false;
+}
